8e542b2a5adb467c74ca4f45fa5a102e34c71ad0
Liangqiong Qu <liangqiqu2-c@my.cityu.edu.hk>
2021-11-24 16:24:15 -0800

    Add files via upload

--- utils/Simple_extraction.py
+++ utils/Simple_extraction.py
@@ -1,1 +1,86 @@
+import pandas as pd
+import string
+import re
+
+
+def extract(txt):
+    txt = txt.encode("ascii", errors="ignore").decode()
+    txt = txt.lower()
+    txt = txt.replace('\n', ' ')
+    txt = txt.replace('\r', ' ')
+    txt = txt.replace('\t', ' ')
+    re1 = '(\\()'  # Any Single Character 1
+    re2 = '.*?'  # Non-greedy match on filler
+    re3 = '(\\))'  # Any Single Character 2
+
+    rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
+    tags = ['clinical indication: ','support devices: ','comparison: ','findings: ', 'impression:', 'critical findings', 'important findings']
+    sections = {'clinical indication: ': ' ','support devices: ': ' ','comparison: ': ' ','findings: ': ' ', 'impression: ': ' '}
+    for t in sections.keys():
+        try:
+            tmp = txt.split(t)[1]
+            for l in tags:
+                if t !=l:
+                    tmp = tmp.split(l)[0]
+                    sections[t] = re.sub(rg, ' ', tmp.split('these findings: ')[0])
+        except:
+            sections[t] = ' '
+    print(sections.keys())
+    return sections
+
+def complex_split(critical):
+    indication= []
+    technique = []
+    comparison = []
+    findings = []
+    impression = []
+    report = []
+    category = []
+    PUI = ['pui', 'person under investigation', 'covid']
+    CP =  ['chest pain']
+    Cough =  ['cough']
+    SOB= ['shortness of breath', 'sob', 'respiratory distress', 'chf', 'congestive, dyspnea', 'volume overload']
+    Infx = ['fever', 'infection', 'sepsis']
+    Neuro = ['AMS', 'headache', 'stroke', 'weakness', 'snycope', 'altered',' mental']
+    Abd=  ['abdominal', 'pelvic', 'vomiting', 'distension'] 
+    flg_pu = 0
+    for i in range(critical.shape[0]):
+        # sections = extract(critical.iloc[i]['report_body'])
+        sections = extract(critical.iloc[i]['report_body_x'])
+        indication.append(sections['clinical indication: '])
+        technique.append(sections['support devices: '])
+        comparison.append(sections['comparison: '])
+        findings.append(sections['findings: '])
+        impression.append(sections['impression: '])
+        if bool([ele for ele in PUI if(ele in sections['clinical indication: '])]):
+            category.append('PUI')
+            flg_pu = 1
+        elif bool([ele for ele in SOB if(ele in sections['clinical indication: '])]) :
+            category.append('SOB')
+            flg_pu = 1
+        elif bool([ele for ele in Cough if(ele in sections['clinical indication: '])]) :
+            category.append('Cough')
+            flg_pu = 1
+        elif bool([ele for ele in Infx if(ele in sections['clinical indication: '])]) :
+            category.append('Infx')
+            flg_pu = 1
+        elif bool([ele for ele in CP if(ele in sections['clinical indication: '])]) :
+            category.append('CP')
+            flg_pu = 1
+        elif bool([ele for ele in Neuro if(ele in sections['clinical indication: '])]) :
+            category.append('Neuro')
+            flg_pu = 1
+        elif bool([ele for ele in Abd if(ele in sections['clinical indication: '])]) :
+            category.append('abd')
+            flg_pu = 1
+        else:
+            category.append('Others')
+        flg_pu = 0
+    critical['indication'] = indication
+    critical['support_devices'] = technique
+    critical['comparison'] = comparison
+    critical['indication_category'] = category
+    critical['impression'] = impression
+    critical['findings'] = findings
+    return critical
 
